86d2de957af6852badec52ee59f59099e74de2f5
Thomas Kuhlmann <thomas.kuhlmann@thk-systems.de>
2026-06-06 19:11:20 +0200

    Add docs and examples, fixed path in systemd unit, do replaces not in repo files, remove version from binary

--- release/release.sh
+++ release/release.sh
@@ -6,27 +6,35 @@ VERSION="${VERSION:-0.9.0}"
 PACKAGE_VERSION="${PACKAGE_VERSION:-$(printf '%s' "$VERSION" | sed 's/-/~/g')}"
 OUT_DIR="$ROOT_DIR/dist"
 BUILD_DIR="$ROOT_DIR/.build"
-
-CLIENT_SRC="$ROOT_DIR/systemd-service-toggle/main.go"
-SERVER_SRC="$ROOT_DIR/systemd-service-toggled/main.go"
+SRC_DIR="$BUILD_DIR/src"
 
 CLIENT_NAME="systemd-service-toggle-client"
 SERVER_NAME="systemd-service-toggle-server"
 
+rm -rf "$OUT_DIR" "$BUILD_DIR"
+mkdir -p "$OUT_DIR" "$SRC_DIR"
+
+copy_sources() {
+	cp -R "$ROOT_DIR/common" "$SRC_DIR/"
+	cp -R "$ROOT_DIR/systemd-service-toggle" "$SRC_DIR/"
+	cp -R "$ROOT_DIR/systemd-service-toggled" "$SRC_DIR/"
+	cp "$ROOT_DIR/go.work" "$SRC_DIR/"
+	cp "$ROOT_DIR/go.work.sum" "$SRC_DIR/"
+}
+
 set_source_version() {
 	version="$1"
-	sed -i "s/var version = \"[^\"]*\"/var version = \"$version\"/" "$CLIENT_SRC" "$SERVER_SRC"
+	sed -i "s/var version = \"[^\"]*\"/var version = \"$version\"/" \
+		"$SRC_DIR/systemd-service-toggle/main.go" \
+		"$SRC_DIR/systemd-service-toggled/main.go"
 }
 
-cleanup() {
-	set_source_version dev
+package_unit_file() {
+	unit_file="$BUILD_DIR/systemd-service-toggled.service"
+	cp "$ROOT_DIR/systemd-service-toggled.service" "$unit_file"
+	sed -i 's#/usr/local/bin/systemd-service-toggled#/usr/bin/systemd-service-toggled#' "$unit_file"
+	printf '%s\n' "$unit_file"
 }
-
-trap cleanup EXIT INT TERM
-set_source_version "$VERSION"
-
-rm -rf "$OUT_DIR" "$BUILD_DIR"
-mkdir -p "$OUT_DIR" "$BUILD_DIR"
 
 need() {
 	if ! command -v "$1" >/dev/null 2>&1; then
@@ -41,9 +49,12 @@ build_go() {
 	goarch="$3"
 	out="$4"
 
-	GOOS="$goos" GOARCH="$goarch" CGO_ENABLED=0 go build \
-		-ldflags "-s -w" \
-		-o "$out" "$module"
+	(
+		cd "$SRC_DIR"
+		GOOS="$goos" GOARCH="$goarch" CGO_ENABLED=0 go build \
+			-ldflags "-s -w" \
+			-o "$out" "$module"
+	)
 }
 
 control_file() {
@@ -63,23 +74,39 @@ Description: $description
 CONTROL
 }
 
+install_doc() {
+	package="$1"
+	pkg_dir="$2"
+	doc_dir="$pkg_dir/usr/share/doc/$package"
+
+	mkdir -p "$doc_dir"
+	install -m 0644 "$ROOT_DIR/README.md" "$doc_dir/"
+	install -m 0644 "$ROOT_DIR/SECURITY.md" "$doc_dir/"
+	install -m 0644 "$ROOT_DIR/CONTRIBUTING.md" "$doc_dir/"
+	install -m 0644 "$ROOT_DIR/LICENSE" "$doc_dir/copyright"
+	cp -R "$ROOT_DIR/cert-generation-examples" "$doc_dir/"
+	cp -R "$ROOT_DIR/config-examples" "$doc_dir/"
+}
+
 build_deb() {
 	package="$1"
 	binary="$2"
-	arch="$3"
-	description="$4"
-	with_service="$5"
+	install_name="$3"
+	arch="$4"
+	description="$5"
+	with_service="$6"
 
 	pkg_dir="$BUILD_DIR/${package}_${PACKAGE_VERSION}_${arch}"
 	rm -rf "$pkg_dir"
 	mkdir -p "$pkg_dir/DEBIAN" "$pkg_dir/usr/bin"
 
-	install -m 0755 "$binary" "$pkg_dir/usr/bin/"
+	install -m 0755 "$binary" "$pkg_dir/usr/bin/$install_name"
 	control_file "$package" "$arch" "$description" "$pkg_dir/DEBIAN/control"
+	install_doc "$package" "$pkg_dir"
 
 	if [ "$with_service" = "yes" ]; then
-		mkdir -p "$pkg_dir/lib/systemd/system"
-		install -m 0644 "$ROOT_DIR/systemd-service-toggled.service" "$pkg_dir/lib/systemd/system/"
+		mkdir -p "$pkg_dir/usr/lib/systemd/system"
+		install -m 0644 "$UNIT_FILE" "$pkg_dir/usr/lib/systemd/system/"
 	fi
 
 	dpkg-deb --root-owner-group --build "$pkg_dir" "$OUT_DIR/${package}_${PACKAGE_VERSION}_${arch}.deb"
@@ -97,6 +124,10 @@ need go
 need dpkg-deb
 need alien
 
+copy_sources
+set_source_version "$VERSION"
+UNIT_FILE="$(package_unit_file)"
+
 for arch in amd64 arm64; do
 	client_bin="$BUILD_DIR/${CLIENT_NAME}_${arch}"
 	server_bin="$BUILD_DIR/${SERVER_NAME}_${arch}"
@@ -104,9 +135,9 @@ for arch in amd64 arm64; do
 	build_go ./systemd-service-toggle linux "$arch" "$client_bin"
 	build_go ./systemd-service-toggled linux "$arch" "$server_bin"
 
-	build_deb "$CLIENT_NAME" "$client_bin" "$arch" \
+	build_deb "$CLIENT_NAME" "$client_bin" "systemd-service-toggle" "$arch" \
 		"mTLS-protected remote systemd service toggle client" "no"
-	build_deb "$SERVER_NAME" "$server_bin" "$arch" \
+	build_deb "$SERVER_NAME" "$server_bin" "systemd-service-toggled" "$arch" \
 		"mTLS-protected remote systemd service toggle service" "yes"
 
 	build_rpm_from_deb "$OUT_DIR/${CLIENT_NAME}_${PACKAGE_VERSION}_${arch}.deb"
